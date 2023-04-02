26780b8d5f47262b13aab309dc798deaf1c09d7a
Aryan Sharma <72523773+aryn117@users.noreply.github.com>
2023-04-02 11:29:58 +0530

    pwa fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,18 +3,33 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" data-theme="cupcake">
-
       <meta
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=yes, user-scalable=no, viewport-fit=cover"
       />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png"></link>
-        <meta name="theme-color" content="#EF9FBC" />
+      <meta
+        name="description"
+        content="A Torrent Aggregator Site With Minimal Interface"
+      />
+      <meta
+        name="keywords"
+        content="Torrents, 1337x, The Pirate Bay, BitTorrent"
+      />
+      <meta name="author" content="@aru" />
+
+      <link rel="manifest" href="/manifest.json" />
+      <link
+        rel="shortcut icon"
+        href="/images/favicon.ico"
+        type="image/x-icon"
+      />
+
+      <link rel="apple-touch-icon" href="/images/apple-touch-icon.png"></link>
+      <meta name="theme-color" content="#EF9FBC" />
       <Head />
       <body>
         <Main />
-        <NextScript />        
+        <NextScript />
       </body>
     </Html>
   );
